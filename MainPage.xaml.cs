b02145c1b3f3d2ab2fedf8c598ec03e035f714e0
Alderi-Junior <alderipasajunior@hotmail.com>
2025-06-22 16:50:00 -0300

    Start: Commit inical contendo a parte visual do projeto

---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using PalavraMestre.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+
+namespace PalavraMestre;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+        //PalavrasListView.ItemsSource = palavrasRegistradas;
+    }
+
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+
+#if WINDOWS
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+using WinRT.Interop; // Necessário para GetWindowHandle e GetWindowIdFromWindow
+#endif
+
+namespace PalavraMestre
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+#if WINDOWS
+builder.ConfigureLifecycleEvents(events =>
+{
+    events.AddWindows(windows =>
+    {
+        windows.OnWindowCreated(window =>
+        {
+            IntPtr hwnd = WindowNative.GetWindowHandle(window);
+            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd); // <- nome completo
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+
+            appWindow.SetPresenter(AppWindowPresenterKind.Overlapped);
+            var displayArea = DisplayArea.GetFromWindowId(windowId, DisplayAreaFallback.Primary);
+            appWindow.MoveAndResize(displayArea.WorkArea);
+
+        });
+    });
+});
+#endif
+
+            return builder.Build();
+        }
+    }
+}
